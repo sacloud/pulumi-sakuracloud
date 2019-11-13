164d6c0cfe3e1b7f43e4ea68ec1eb583ba572682
febc.yamamoto <yamamoto.febc@gmail.com>
2019-11-13 16:28:42 +0900

    Add .NET core(csharp) example

---
+++ examples/minimum-cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Pulumi;
+using Pulumi.Sakuracloud;
+
+class Program
+{
+    static Task<int> Main()
+    {
+        return Deployment.RunAsync(() =>
+        {
+            var sw = new Switch("from-pulumi-with-csharp", new SwitchArgs
+            {
+                Name = "from-pulumi-with-csharp",
+                Tags = new string[] { "tag1", "tag2" },
+                Description = "this resource was created by pulumi"
+            });
+            return new Dictionary<string, object>
+            {
+                { "Name", sw.Name }
+            };
+        });
+    }
+}
